7021d30319716254b1bb1cb8ef7eb0c06713a48e
Jose Octavio de Castro Neves Jr <joc@fullsquad.com.br>
2019-10-03 16:50:15 -0300

    fixed mongo connection string to heroku style

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const SlackStrategy = require("passport-slack").Strategy;
 
 
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
